ba88af012ffd7676d547b84dc61189be7a6aa953
Ivan Cheung <ivan.j.cheung@gmail.com>
2016-01-22 17:33:12 -0600

    create layout

---
+++ app/src/main/java/com/icheung/portfolio/MainActivity.java
@@ -0,0 +1,21 @@
+package com.icheung.portfolio;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void comingSoon(View v){
+        CharSequence app = ((Button) v).getText();
+        Toast.makeText(this, getString(R.string.link_coming, app), Toast.LENGTH_SHORT).show();
+    }
+}
